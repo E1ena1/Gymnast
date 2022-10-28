dbddbfba20ac7819999d125efa8f2fa91312d513
ElenaK <kalinakrasnaya33@gmail.com>
2022-10-28 12:10:07 +0300

    added modal

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init();
+
+
+document.querySelector('.header__menu').addEventListener('click', function (){
+    document.getElementById('modal').style.top = '0px';
+})
+
+
+document.querySelector('.modal__close').addEventListener('click', function () {
+    document.getElementById('modal').style.top = '-9000px';
+    }
+)
